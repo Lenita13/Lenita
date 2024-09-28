c6a6018d42feea39785aa697e230d65b06ee9c40
André Miyazawa <andre08.m@usp.br>
2024-09-28 20:58:11 -0300

    new Website v1.0

---
+++ src/smoothScroll.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
