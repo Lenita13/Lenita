cd2fdd9c22d6e5a6ed2ee1eaa9bcf604de629166
André Miyazawa <andre08.m@usp.br>
2024-09-29 21:01:07 -0300

    new Website v1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundColor: {
         'custom-purple': '#8e65c0',
-        'custom-blue': '#e6f0f2', // Blueish color
+        'custom-blue': '#e6f0f2',
       },
       textColor: {
+        'custom-purple': '#8e65c0',
+      },
+      borderColor: {
         'custom-purple': '#8e65c0',
       },
       fontFamily: {
