93258e070ae24bbba21634c1221502b1dae98982
André Miyazawa <andre08.m@usp.br>
2024-10-01 20:25:03 -0300

    Including better projects view

--- src/smoothScroll.js
+++ src/smoothScroll.js
@@ -31,3 +31,47 @@ document.getElementById('menu-button2').addEventListener('click', function() {
         menuButton.classList.remove('hidden');
     }
 });
+
+////////////////////
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => {
+    track.dataset.mouseDownAt = e.clientX || e.touches[0].clientX;
+};
+
+const handleOnUp = () => {
+    track.dataset.mouseDownAt = "0";  
+    track.dataset.prevPercentage = track.dataset.percentage;
+};
+
+const handleOnMove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const clientX = e.clientX || e.touches[0].clientX;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - clientX,
+          maxDelta = window.innerWidth;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+          nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+          maxPercentage = window.innerWidth <= 768 ? -315 : -100,
+          nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), maxPercentage);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.style.transform = `translate(${nextPercentage}%, 0%)`;
+
+    for(const image of track.getElementsByClassName("image")) {
+        image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+};
+
+/* -- Add event listeners for both mouse and touch events -- */
+
+window.addEventListener('mousedown', handleOnDown);
+window.addEventListener('touchstart', handleOnDown);
+
+window.addEventListener('mouseup', handleOnUp);
+window.addEventListener('touchend', handleOnUp);
+
+window.addEventListener('mousemove', handleOnMove);
+window.addEventListener('touchmove', handleOnMove);
