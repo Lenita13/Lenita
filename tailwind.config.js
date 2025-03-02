381654c5056a8a07da173eea83eb5736595f0d1c
Lenita <lenitalemos08@gmail.com>
2025-03-02 12:12:59 -0300

    muedi a cor de fundo e ft de perfil

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'custom-purple': '#8e65c0',
+        'custom-purple': '#7742cc',
         'custom-blue': '#e6f0f2',
       },
       textColor: {
-        'custom-purple': '#8e65c0',
+        'custom-purple': '#7742cc',
       },
       borderColor: {
-        'custom-purple': '#8e65c0',
+        'custom-purple': '#7742cc',
       },
       fontFamily: {
         'galano': ['Galano Classic', 'sans-serif'],
